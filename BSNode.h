124de01f30b243cbc5f9c177d42793dfafbb313a
Mar Real <mreaosc@upv.edu.es>
2025-12-04 17:56:38 +0100

    Añadida implementación de la clase BSNode

---
+++ BSNode.h
@@ -0,0 +1,31 @@
+
+#ifndef BSNODE_H
+#define BSNODE_H
+
+#include <ostream>
+
+ 
+ template <typename T>
+class BSNode {
+public:
+    T elem;
+    BSNode<T>* left;
+    BSNode<T>* right;
+
+    BSNode(T elem, BSNode<T>* left = nullptr, BSNode<T>* right = nullptr)
+        : elem(elem), left(left), right(right) {}
+
+
+};
+
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const BSNode<T>& bsn) {
+
+    out << bsn.elem;
+    return out;
+}
+
+#endif // BSNODE_H
+
+
